b010b3ac27b8884db261f3fff29e290b13d34f34
magiruuvelvet <magiruuvelvet@naver.com>
2019-07-04 12:26:51 +0200

    tests: update logger tests

--- Tests/logger-test.hpp
+++ Tests/logger-test.hpp
@@ -3,13 +3,13 @@
 void logger_test()
 {
     // result: "hello world 3 3.30 3" with the proper prefix
-    LOG("hello %s %i %.2f %i", "world", 3, 3.3, 3);
-    LOG_INFO("hello %s %i %.2f %i", "world", 3, 3.3, 3);
-    LOG_WARNING("hello %s %i %.2f %i", "world", 3, 3.3, 3);
-    LOG_ERROR("hello %s %i %.2f %i", "world", 3, 3.3, 3);
-    LOG_FATAL("hello %s %i %.2f %i", "world", 3, 3.3, 3);
+    LOG("hello {} {} {:.2f} {}", "world", 3, 3.3, 3);
+    LOG_INFO("hello {} {} {:.2f} {}", "world", 3, 3.3, 3);
+    LOG_WARNING("hello {} {} {:.2f} {}", "world", 3, 3.3, 3);
+    LOG_ERROR("hello {} {} {:.2f} {}", "world", 3, 3.3, 3);
+    LOG_FATAL("hello {} {} {:.2f} {}", "world", 3, 3.3, 3);
 
     // test string formatting
-    auto res = format("hello %s %i %.2f %i", "world", 3, 3.3, 3);
+    auto res = format("hello {} {} {:.2f} {}", "world", 3, 3.3, 3);
     LOG(res);
 }
