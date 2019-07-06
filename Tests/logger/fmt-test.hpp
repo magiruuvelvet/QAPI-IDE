664efe83bd83332ef4d49689f05451fad40553c0
magiruuvelvet <magiruuvelvet@naver.com>
2019-07-06 11:14:45 +0200

    tests: more formatting tests

--- Tests/logger/fmt-test.hpp
+++ Tests/logger/fmt-test.hpp
@@ -11,5 +11,19 @@ go_bandit([]{
             const auto res = format("hello {} {} {:.2f} {}", "world", 3, 3.3, 3);
             AssertThat(res, Equals("hello world 3 3.30 3"));
         });
+
+        // booleans should be formatted as such, and not as numbers (1, 0)
+        // if this test fails on your machine your compiler might have a bug
+        // bug reports should go to https://github.com/fmtlib/fmt in this case
+        it("[fmt bool]", [&]{
+            const auto res = format("{} {}", true, false);
+            AssertThat(res, Equals("true false"));
+        });
+
+        // test hexadecimal formatting
+        it("[fmt hex]", [&]{
+            const auto res = format("0x{:X}", 0xFF);
+            AssertThat(res, Equals("0xFF"));
+        });
     });
 });
