805b33a12d1e7cc79850fa103bf61754edc878fc
magiruuvelvet <magiruuvelvet@naver.com>
2019-07-06 11:37:55 +0200

    request-lib: rearrange logger support

---
+++ RequestLib/requestlib/log.hpp
@@ -0,0 +1,7 @@
+#ifndef REQUESTLIB_LOGGER_SUPPORT_HPP
+#define REQUESTLIB_LOGGER_SUPPORT_HPP
+
+#include <requestlib/logger_support/request.hpp>
+#include <requestlib/logger_support/response.hpp>
+
+#endif // REQUESTLIB_LOGGER_SUPPORT_HPP
