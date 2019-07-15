c63f088f0f2565e7fb3e5a2207c87dabe647cce8
magiruuvelvet <magiruuvelvet@naver.com>
2019-07-15 14:33:02 +0200

    tests: more cling tests

--- Tests/script/cpp.hpp
+++ Tests/script/cpp.hpp
@@ -7,7 +7,7 @@ using namespace bandit;
 
 go_bandit([]{
     describe("C++", []{
-        it("[value binding]", [&]{
+        it("[static value binding]", [&]{
             Script scr(Script::Cpp);
             std::string output;
             scr.setScriptContents("out << number << string << std::endl;");
@@ -16,6 +16,46 @@ go_bandit([]{
             AssertThat(scr.bindValue("string", std::string{"hello"}), Equals(true));
             AssertThat(scr.evaluate(output), Equals(0));
             AssertThat(output, Equals("42hello\n"));
+        });
+
+        it("[error stream]", [&]{
+            Script scr(Script::Cpp);
+            std::string output, error;
+            scr.setScriptContents(R"(err << "this is an error" << std::endl;)");
+
+            AssertThat(scr.evaluate(output, error), Equals(0));
+            AssertThat(output, Equals(""));
+            AssertThat(error, Equals("this is an error\n"));
+        });
+
+        it("[return value]", [&]{
+            Script scr(Script::Cpp);
+            scr.setScriptContents("return 42;");
+            auto ret = scr.evaluate();
+            AssertThat(ret, Equals(42));
+        });
+
+        it("[if statement]", [&]{
+            Script scr(Script::Cpp);
+            std::string output;
+            scr.setScriptContents(R"(
+                if (number == 1)
+                {
+                    return 1;
+                }
+                else if (number == 2)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 3;
+                }
+            )");
+            scr.bindValue("number", 2);
+            auto ret = scr.evaluate(output);
+            AssertThat(output, Equals(""));
+            AssertThat(ret, Equals(2));
         });
     });
 });
