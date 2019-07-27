b1a7ff6b536c60516c949b319308faa67a45f7f7
magiruuvelvet <magiruuvelvet@naver.com>
2019-07-27 17:00:05 +0200

    tests: add some tests regarding case sensitivity

--- Tests/request-lib/headers.hpp
+++ Tests/request-lib/headers.hpp
@@ -16,11 +16,34 @@ go_bandit([]{
             AssertThat(req.hasHeader("Test"), Equals(true));
         });
 
+        benchmark_it("[hasHeader case insensitive]", [&]{
+            Request req("");
+            AssertThat(req.hasHeader("Test"), Equals(false));
+            req.setHeader("Test", "");
+            AssertThat(req.hasHeader("TEST"), Equals(true));
+        });
+
         benchmark_it("[removeHeader]", [&]{
             Request req("");
             req.setHeader("Test", "");
             req.removeHeader("Test");
             AssertThat(req.hasHeader("Test"), Equals(false));
+        });
+
+        benchmark_it("[removeHeader case insensitive]", [&]{
+            Request req("");
+            req.setHeader("Test", "");
+            req.removeHeader("TEST");
+            AssertThat(req.hasHeader("Test"), Equals(false));
+        });
+
+        benchmark_it("[header values]", [&]{
+            Request req("");
+            req.setHeader("Set-Cookie", "cookie 1");
+            req.setHeader("Set-Cookie", "cookie 2");
+            AssertThat(req.getHeaderValues("Set-Cookie"), Equals(std::list<std::string>{"cookie 1", "cookie 2"}));
+            req.removeHeader("Set-Cookie");
+            AssertThat(req.hasHeader("set-cookie"), Equals(false));
         });
     });
 });
